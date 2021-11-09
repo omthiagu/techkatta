b70a20ac87f70373137f9257dbb021ff420e0391
Thiagarajan Nagarathinam <omthiagu@gmail.com>
2021-11-09 12:27:22 -0800

    To support storing and fetching unique user data

--- src/main/java/com/ohiohealth/techkatta/service/UserService.java
+++ src/main/java/com/ohiohealth/techkatta/service/UserService.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Date;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -20,13 +21,18 @@ public class UserService {
 	/**
 	 * This method would fetch user data from a file.
 	 * 
+	 * @param userId String
 	 * @return User 
 	 */
-	public User fetchStoredUserData() {
+	public User fetchStoredUserData(String userId) {
 		logger.log(Level.INFO, "fetchUserData Begin");
 
 		User userObj = null;
-		Path path = Paths.get("user.json");
+
+		String fileName = "user-"+userId+".json";
+		logger.info("userId="+userId+", fileName="+fileName);
+
+		Path path = Paths.get(fileName);
 		try {
 			String userStringObj = Files.readAllLines(path).get(0);
 
@@ -53,12 +59,18 @@ public class UserService {
 	 * This method would store user data into a file.
 	 * 
 	 * @param userObj User
-	 * @return Boolean
+	 * @return String userId
 	 */
-	public Boolean storeUserData(User userObj) {
-		logger.log(Level.INFO, "storeUserData Begin");
+	public String storeUserData(User userObj) {
+		logger.info("storeUserData Begin");
 
-		Path path = Paths.get("user.json");
+		String userId= UUID.randomUUID()+"-"+System.currentTimeMillis();
+
+		String fileName = "user-"+userId+".json";
+
+		logger.info("userId="+userId+", fileName="+fileName);
+
+		Path path = Paths.get(fileName);
 		try {
 			ObjectMapper objMapper = new ObjectMapper();
 			byte[] strToBytes = objMapper.writeValueAsBytes(userObj);
@@ -67,8 +79,9 @@ public class UserService {
 			logger.log(Level.SEVERE, "Exception occurred while writting user data to the file=" + path.getFileName(),
 					ioexception);
 		}
-		logger.log(Level.INFO, "storeUserData End");
-		return Boolean.TRUE;
+		logger.info("storeUserData End");
+
+		return userId;
 	}
 
 }
